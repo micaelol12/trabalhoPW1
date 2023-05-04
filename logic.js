a3f90af6e565bd8faee72e26503f72a12a5e69cd
Micael Luan Conti <mlconti@furb.br>
2023-05-04 19:33:11 -0300

    3 page

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@ const USER_NAME_ID = "user_name";
 const USER_IMAGE_ID = "user_image";
 const HIDDEN_CLASS = "hidden";
 const PAGINA_INICIAL = "index.html";
-
+const PAGINA_PERFIL = "perfil.html"
 const FORM_ID = 'form'
 const LOGIN_ID = "login";
 const PASSWORD_ID = 'password'
@@ -21,28 +21,44 @@ const logado = () => !!getUser();
 const saveUser = (user) => localStorage.setItem('user',JSON.stringify(user))
 
 const GerenciaAvatar = () => {
-  if (!logado()) {
-    userName.innerText = "Usuário não autenticado";
-    userAvatar.classList.add(HIDDEN_CLASS);
-  } else {
-    userName.innerText = getUser().login;
-    userAvatar.classList.remove(HIDDEN_CLASS);
+  if(userName && userAvatar){
+    if (!logado()) {
+      userName.innerText = "Usuário não autenticado";
+      userAvatar.classList.add(HIDDEN_CLASS);
+    } else {
+      userName.innerText = getUser().login;
+      userAvatar.classList.remove(HIDDEN_CLASS);
+    }
   }
+  
 };
 
-
+if(form){ 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
+   
   const user = {
     login: loginInput.value,
     password: passwordInput.value
   }
+  if(!user.login || !user.login){
+    window.alert("Informe os dados de autenticação")
+    return;   
+  }
   saveUser(user)
   GerenciaAvatar()
+  vaiParaPaginaInicial()
 })
+}
+
 
 function vaiParaPaginaInicial() {
   window.location.href = PAGINA_INICIAL;
+}
+
+
+function vaiParaPerfil() {
+  window.location.href = PAGINA_PERFIL;
 }
 
 GerenciaAvatar();
